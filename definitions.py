b68555c4f3107a3ae5527ac97a0756fc55cd9115
videogamerm <75590266+videogamerm@users.noreply.github.com>
2021-07-14 08:40:09 -0400

    Delete definitions.py

--- definitions.py
+++
@@ -1,7 +0,0 @@
-import random
-class enemy:
-  def __init__(type, hp, damge, attack1, attack2):
-    hpp = random.randint(hp-3,hp+5)
-    damages = random.randint(damage+5,damage+5)
-    attackone.name = attack1
-    attacktwo.name = attack2
